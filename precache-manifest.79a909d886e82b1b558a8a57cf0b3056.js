bfd93fd48a879b4bf74666e1860e970c91085114
virinchi123 <raovirinchi123@gmail.com>
2020-05-28 10:43:45 +0530

    Updates

---
+++ precache-manifest.79a909d886e82b1b558a8a57cf0b3056.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "423968d51befd7c5f656a15d7263e30b",
+    "url": "/GoReadMistborn/index.html"
+  },
+  {
+    "revision": "36e735593403d07aa0cd",
+    "url": "/GoReadMistborn/static/css/main.d9329d7d.chunk.css"
+  },
+  {
+    "revision": "849ea1133c3373b98e94",
+    "url": "/GoReadMistborn/static/js/2.bf83892a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/GoReadMistborn/static/js/2.bf83892a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36e735593403d07aa0cd",
+    "url": "/GoReadMistborn/static/js/main.03390062.chunk.js"
+  },
+  {
+    "revision": "06a32822e30961cb3d6b",
+    "url": "/GoReadMistborn/static/js/runtime-main.dceba3f4.js"
+  }
+]);
